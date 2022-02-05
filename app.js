3afe0d61c304ffa6068c76eb6340a39cc76c2be9
Mo2024 <mohdosama2025@gmail.com>
2022-02-05 08:21:18 +0300

    Flash added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 mongoose.connect('mongodb://localhost:27017/yelpCamp')
     .then(() => {
@@ -24,7 +26,28 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.msg = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
 
 app.use('/campgrounds', require('./routes/campgrounds'))
 app.use('/campgrounds/:id/reviews', require('./routes/reviews'))
@@ -40,7 +63,6 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    console.log(err)
     res.status(statusCode).render('error', { err })
 })
 app.listen(3000, () => {
