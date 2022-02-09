691272f0a31d8233c782ece4f1659f69913ee2c4
Mo2024 <mohdosama2025@gmail.com>
2022-02-09 14:52:55 +0300

    Before copy paste

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -14,10 +11,14 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
-// const dotenv = require('dotenv');
-// dotenv.config({ path: './.env' })
-const dbUrl = process.env.DB_URL
+const sanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
+require('dotenv').config();
+// if (process.env.NODE_ENV !== "production") {
+//     require('dotenv').config();
+// }
 
+const dbUrl = process.env.DB_URL
 mongoose.connect(dbUrl)
     .then(() => {
         console.log("Connected to database...")
@@ -36,8 +37,10 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(sanitize())
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
@@ -45,10 +48,56 @@ const sessionConfig = {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true
+        // secure: true
     }
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/yelp-camp2024/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session())
@@ -74,14 +123,14 @@ app.get('/', (req, res) => {
 });
 
 
-// app.all('*', (req, res, next) => {
-//     next(new ExpressError('Page not Found', 404))
-// })
-// app.use((err, req, res, next) => {
-//     const { statusCode = 500 } = err;
-//     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-//     res.status(statusCode).render('error', { err })
-// })
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not Found', 404))
+})
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
 app.listen(3000, () => {
     console.log("Listening to port 3000")
 });
